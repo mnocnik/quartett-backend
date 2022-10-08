2c7986636c305aaf5ecac1f0cb8070d7b757d9da
Mario Nocnik <mario.nocnik@xaidat.com>
2022-10-08 21:07:48 +0200

    KeyCloak: works

--- src/main/java/work/nocnik/cards/angularbackend/graphql/VehicleQueryResolver.java
+++ src/main/java/work/nocnik/cards/angularbackend/graphql/VehicleQueryResolver.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import work.nocnik.cards.angularbackend.database.entity.VehicleEntity;
 import work.nocnik.cards.angularbackend.database.repository.VehicleRepository;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @Controller
@@ -14,6 +15,7 @@ import java.util.List;
 public class VehicleQueryResolver {
   private final VehicleRepository vehicleRepository;
 
+  @RolesAllowed("DEV")
   @QueryMapping
   public List<VehicleEntity> vehicles() {
     return this.vehicleRepository.findAll();
