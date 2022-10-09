839423a3d38f6c0802e33e1d986a4f47c2970245
Mario Nocnik <mario.nocnik@xaidat.com>
2022-10-09 16:16:24 +0200

    GraphQL: Flux - CleanUp

--- src/main/java/work/nocnik/cards/angularbackend/graphql/VehicleResolver.java
+++ src/main/java/work/nocnik/cards/angularbackend/graphql/VehicleResolver.java
@@ -5,6 +5,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import work.nocnik.cards.angularbackend.database.entity.VehicleEntity;
 import work.nocnik.cards.angularbackend.database.repository.VehicleRepository;
 
@@ -18,7 +19,12 @@ public class VehicleResolver {
 
   @RolesAllowed("DEV")
   @QueryMapping
-  public Flux<VehicleEntity> vehicles(@Argument final UUID typeUUID) {
+  public Mono<VehicleEntity> vehicle(@Argument final UUID uuid) {
+    return Mono.justOrEmpty(this.vehicleRepository.findByUuid(uuid));
+  }
+
+  @QueryMapping
+  public Flux<VehicleEntity> vehiclesByType(@Argument final UUID typeUUID) {
     if (typeUUID == null) {
       return Flux.fromIterable(this.vehicleRepository.findAll());
     } else {
